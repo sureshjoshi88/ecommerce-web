2bbee3da5a6d5e77353985f3d6eb5cd3445d0ee2
sureshjoshi88 <sj3795789@gmail.com>
2025-02-25 16:03:09 +0530

    add new styling from

--- sign-in-pages/script.js
+++ sign-in-pages/script.js
@@ -1,21 +1,42 @@
-function inputdata(){
+function inputdata() {
     let input = document.getElementById("number-input");
     let value = input.value;
-    if(value.length!=10){
+    if (value.length != 10) {
+        input.focus()
+        input.classList.add("form-control-after")
+
         alert("mobile number must be of 10 digit")
-        input.classList.add("form-control-after")
-    }else if(value[0]!="9" && value[0]!="8" && value[0]!="7" && value[0]!="6"){
+    } else if (value[0] != "9" && value[0] != "8" && value[0] != "7" && value[0] != "6") {
         alert("mobile number must start with 6,7,8,9")
+        input.focus()
         input.classList.add("form-control-after")
 
-    }else{
+    } else {
         input.classList.remove("form-control-after");
+        input.classList.add("form-control-active")
 
+
+    }
+
+    let password = document.getElementById("password-input")
+    let passwordvalue = password.value;
+    if (passwordvalue.length <= 8) {
+        alert("plese enter 8 number password")
+        password.focus();
+        password.classList.add("form-control-after")
+    } else if (passwordvalue.length >= 8) {
+        alert("good")
     }
 }
 
-// let input =  document.getElementById("exampleInputEmail1");
-// input.addEventListener('focus',function(e){
-//     console.log("clicked")
-//     this.style.boxShadow = "0 0 0 .25rem red"
-//  })
+
+let forms = document.getElementById("sumbit-form")
+function sumbit(event) {
+    event.preventDefault();
+}
+forms.addEventListener("submit", sumbit)
+let input = document.getElementById("number-input");
+input.addEventListener('focus', function (e) {
+    this.style.boxShadow = "0 0 0 .25rem red"
+})
+
