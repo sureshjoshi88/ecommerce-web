673e5c96ea56b569cfa99fdaeebf27dbe3f955d6
sureshjoshi88 <sj3795789@gmail.com>
2025-02-25 14:48:58 +0530

    add new lojics

--- sign-in-pages/script.js
+++ sign-in-pages/script.js
@@ -1,3 +1,21 @@
 function inputdata(){
-    let input = document.getElementById("number-input")
+    let input = document.getElementById("number-input");
+    let value = input.value;
+    if(value.length!=10){
+        alert("mobile number must be of 10 digit")
+        input.classList.add("form-control-after")
+    }else if(value[0]!="9" && value[0]!="8" && value[0]!="7" && value[0]!="6"){
+        alert("mobile number must start with 6,7,8,9")
+        input.classList.add("form-control-after")
+
+    }else{
+        input.classList.remove("form-control-after");
+
+    }
 }
+
+// let input =  document.getElementById("exampleInputEmail1");
+// input.addEventListener('focus',function(e){
+//     console.log("clicked")
+//     this.style.boxShadow = "0 0 0 .25rem red"
+//  })
