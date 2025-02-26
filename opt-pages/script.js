8b0f80b1e68f61217974d5c2c20e5181fa4590b0
sureshjoshi88 <sj3795789@gmail.com>
2025-02-26 12:40:53 +0530

    add new js lojic

---
+++ opt-pages/script.js
@@ -0,0 +1,8 @@
+let otp1 = document.getElementById("otp-1");
+let otp2 = document.getElementById("otp-2");
+let otp3 = document.getElementById("otp-3");
+let otp4 = document.getElementById("otp-4");
+let otp5 = document.getElementById("otp-5");
+let otp6 = document.getElementById("otp-6");
+
+
