88527bc7adcb5416e227ef8b33a912752bbaeccd
sureshjoshi88 <sj3795789@gmail.com>
2025-02-26 12:50:54 +0530

    add ne lojic

--- opt-pages/script.js
+++ opt-pages/script.js
@@ -6,3 +6,11 @@ let otp5 = document.getElementById("otp-5");
 let otp6 = document.getElementById("otp-6");
 
 
+otp1.addEventListener("input",(e)=>{
+    if(e.target.value.length>1){
+        document.getElementById("otp-1").value = e.target.value[0]
+    }else if(e.target.value.length==1){
+        document.getElementById("otp-1").blur();
+        document.getElementById("otp-2").focus();
+    }
+})
