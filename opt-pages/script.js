1761691068324859b32818b7f91da3b9e842d45a
sureshjoshi88 <sj3795789@gmail.com>
2025-02-26 15:25:16 +0530

    add new code for js

--- opt-pages/script.js
+++ opt-pages/script.js
@@ -12,8 +12,6 @@ otp1.addEventListener("input",(e)=>{
     }else if(e.target.value.length==1){
         document.getElementById("otp-1").blur();
         document.getElementById("otp-2").focus();
-    }else if(e.target.value.length==0){
-        document.getElementById("otp-1").blur();
     }
 })
 otp2.addEventListener("input",(e)=>{
@@ -84,7 +82,7 @@ otp5.addEventListener("keydown",(e)=>{
         document.getElementById("otp-4").focus();
     }
 })
-otp6.addEventListener("click",(e)=>{
+otp6.addEventListener("input",(e)=>{
     if(e.target.value.length>1){
         document.getElementById("otp-6").value = e.target.value[0]
     }else if(e.target.value.length==0){
@@ -100,3 +98,29 @@ otp6.addEventListener("keydown",(e)=>{
         document.getElementById("otp-5").focus();
     }
 })
+
+
+
+// for(let i = 1;i<=6;i++){
+//     let otp  =`otp-${[i]}`
+//     let otpx = document.getElementById(otp);
+//     otpx.addEventListener("input",(e)=>{
+//         if(e.target.value.length>1){
+//             document.getElementById(`otp-${[i+1]}`).value = e.target.value[0]
+//         }else if(e.target.value.length==1){
+//             document.getElementById(`otp-${[i]}`).blur();
+//             document.getElementById(`otp-${[i+1]}`).focus();
+
+//         }
+//         else if(e.target.value.length==0){
+//             document.getElementById(`otp-${[i+1]}`).blur();
+//             document.getElementById(`otp-${[i+1]}`).focus();
+//         }
+//     })
+//     otpx.addEventListener("keydown",(e)=>{
+//         if(e.key=="Backspace" && e.target.value.length==0){
+//             document.getElementById(`otp-${[i]}`).blur();
+//             document.getElementById(`otp-${[i-1]}`).focus();
+//         }
+//     })
+// }
