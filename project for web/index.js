e71193025bcefb77e09b850374f3094b8349ba6e
sureshjoshi88 <sj3795789@gmail.com>
2025-03-06 10:04:53 +0530

    debuing

--- project for web/index.js
+++ project for web/index.js
@@ -22,7 +22,7 @@ fetch("https://admin-dev-backend.edvice.in/ping", requestOptions)
     //     console.log(result.error.explanation);
     // }
   })
-  .catch((error) => console.error(error));
+  .catch((error) => console.log(error));
 }
 isuserauthicated();
 
