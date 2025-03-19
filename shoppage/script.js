8180b4f253d04007e846b14bc7c09f6147a16573
sureshjoshi88 <sj3795789@gmail.com>
2025-03-19 16:11:33 +0530

    changes and set data local stroage

--- shoppage/script.js
+++ shoppage/script.js
@@ -1,19 +1,20 @@
 let button = document.getElementById("main-buton-shop")
 button.addEventListener("click",()=>{
-    function setdata(id,img,price){
+    function setdata(id,img,price,price2){
         let cart = JSON.parse(localStorage.getItem("cart"))||[];
         let existing = cart.find(item=> item.id===id)
         if(existing){
             existing.quintity +=1
         }else{
-            cart.push({id,img,price})
+            cart.push({id,img,price,price2})
         }
         localStorage.setItem("cart",JSON.stringify(cart))
         alert("added succefull")
     }
     let imgs = document.getElementById("shop-img").src
     let product = document.getElementById("h5-shop").innerText
-    setdata(1,imgs,product)
+    let product2 = document.getElementById("price-shop").innerText
+    setdata(1,imgs,product,product2)
     getdata()
 
 })
@@ -27,14 +28,19 @@ function getdata(){
         console.log(value);
         
         let maindiv = document.createElement("div")
-        maindiv.classList.add("d-flex","gap-4","align-items-center","p-2")
+        maindiv.classList.add("d-flex","gap-5","align-items-center","p-2")
 
         let img = document.createElement("img")
         img.classList.add("w-25")
-        img.src=  value.img;
+        img.src = value.img;
         
+        let childiv = document.createElement("div")
         let products = document.createElement("h6")
         products.innerText = value.price;
+        let products1 = document.createElement("h6")
+        products1.innerText = value.price2;
+        childiv.appendChild(products)
+        childiv.appendChild(products1)
        
         let delte = document.createElement("button")
         delte.classList.add("border","btn","btn-danger")
@@ -44,7 +50,7 @@ function getdata(){
         getdata();          
         })
         maindiv.appendChild(img)
-        maindiv.appendChild(products)
+        maindiv.appendChild(childiv)
         maindiv.appendChild(delte)
         main.appendChild(maindiv)
 
