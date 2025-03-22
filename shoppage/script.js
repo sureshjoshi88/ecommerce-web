11ce30e6789e4ebc01d74f158dab72c519ea3f55
sureshjoshi88 <sj3795789@gmail.com>
2025-03-22 15:39:02 +0530

    set of format

--- shoppage/script.js
+++ shoppage/script.js
@@ -53,16 +53,16 @@
 //     let parent1 =  e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[1].children[0]
 //     // console.log(parent1);
 //     let parent2 = e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[1].children[2].children[0]
-    
+
 //     console.log(parent2);
-    
-    
+
+
 // })
-function mainfunction(){
+function mainfunction() {
 
     let button = document.querySelectorAll(".main-buton-shop-1")
-    button.forEach((button)=>{
-    
+    button.forEach((button) => {
+
         button.addEventListener("click", (e) => {
             function setdata(id, img, price, price2) {
                 let cart = JSON.parse(localStorage.getItem("cart")) || [];
@@ -81,41 +81,41 @@ function mainfunction(){
             // let product2 = document.getElementById("price-shop").
             let parent = e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[0].children[0].src
             // console.log(parent);
-            
-            let parent1 =  e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[1].children[0].innerText
+
+            let parent1 = e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[1].children[0].innerText
             let parent2 = e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[1].children[2].children[0].innerText
             // console.log(parent2);
-            
+
             let parent3 = e.target.parentElement.id
             // console.log(parent3);
-            
-            setdata(parent3,parent,parent1,parent2)
+
+            setdata(parent3, parent, parent1, parent2)
             // data.map((value, index) => {
             //     setdata(value.id,value.images,value. brand,value.discountprice)
             // })
             getdata()
-        
+
         })
     })
-    
+
     function getdata() {
         let carts = JSON.parse(localStorage.getItem("cart")) || [];
         console.log(carts);
-        
+
         let main = document.getElementById("shop-tofle-div")
         main.innerHTML = "";
         let totalprice = 0;
         carts.map((value, index) => {
-                // console.log(value);
-                
+            // console.log(value);
+
             let maindiv = document.createElement("div")
             maindiv.classList.add("d-flex", "justify-content-between", "align-items-center", "p-2")
             maindiv.id = value.id
-    
+
             let img = document.createElement("img")
             img.classList.add("w-25")
             img.src = value.img;
-    
+
             let childiv = document.createElement("div")
             let products = document.createElement("h6")
             products.innerText = value.price;
@@ -124,30 +124,30 @@ function mainfunction(){
             products1.classList.add("setcolor");
             childiv.appendChild(products)
             childiv.appendChild(products1)
-    
+
             let delte = document.createElement("button")
             delte.classList.add("border", "btn", "btn-danger")
             delte.innerText = "X"
             delte.addEventListener("click", () => {
-                
-            removeItem(value.id)
+
+                removeItem(value.id)
                 getdata();
             })
-           
+
             // console.log(finalPrice);
             let priceText = value.price2 || "0"; // agar price undefined ho to "0" use karein
-        let numericPrice = parseInt(priceText.replace(/[^\d]/g, "")); // 🔹 Remove dots, letters & convert to number
+            let numericPrice = parseInt(priceText.replace(/[^\d]/g, "")); // 🔹 Remove dots, letters & convert to number
 
-        if (!isNaN(numericPrice)) {
-            totalprice += numericPrice;
-        }
+            if (!isNaN(numericPrice)) {
+                totalprice += numericPrice;
+            }
             maindiv.appendChild(img)
             maindiv.appendChild(childiv)
             maindiv.appendChild(delte)
             main.appendChild(maindiv)
         })
         let totalmain = document.createElement("div")
-        totalmain.classList.add("d-flex","p-2","justify-content-between")
+        totalmain.classList.add("d-flex", "p-2", "justify-content-between")
         let totalName = document.createElement("h6")
         totalName.innerText = "Subtotal"
         let totalPrice = document.createElement("h6")
@@ -155,12 +155,12 @@ function mainfunction(){
         totalmain.appendChild(totalName)
         totalmain.appendChild(totalPrice)
         main.appendChild(totalmain)
-        
+
     }
-    function removeItem(id){
+    function removeItem(id) {
         let cart = JSON.parse(localStorage.getItem("cart")) || [];
-         cart = cart.filter((value)=>value.id!=id)
-         localStorage.setItem("cart",JSON.stringify(cart))
+        cart = cart.filter((value) => value.id != id)
+        localStorage.setItem("cart", JSON.stringify(cart))
     }
     getdata();
 }
